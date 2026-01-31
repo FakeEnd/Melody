25637ff8d39477b0719f686dc295e6e3edf364fe
void-echo <Beihai142857@outlook.com>
2026-01-31 14:00:56 +0800

    tiny change

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 import argparse
 import os
-parser = argparse.ArgumentParser()
-parser.add_argument('--gpu', type=int, default=0)
-
 import sys
 from typing import Callable
+
+import torch
+
 import numpy as np
 import time
 
+from echo_logger import dumps_json
 from loguru import logger
 from datetime import datetime, timedelta
 from pathlib import Path
@@ -24,7 +25,8 @@ sys.path.append(str(Path(__file__).parent.parent.parent.parent))
 from _config import pdir
 
 start_time_str = "_" + datetime.now().strftime("%Y%m%d_%H%M%S")
-
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpu', type=int, default=0)
 parser.add_argument('--auto-resume', action='store_true', help='Automatically resume latest checkpoint. If no checkpoint is available, do not set it.')
 parser.add_argument('--project', type=str, default='Melody', help='Base directory for data and checkpoints')
 parser.add_argument('--lab_name', type=str, default=f'GridSearch_{start_time_str}', help='Name of the lab for logging')
@@ -40,7 +42,7 @@ parser.add_argument('--seed', type=int, default=5, help='Random seed, default: 4
 parser.add_argument('--genome_path', type=str,
                     default=pdir + '/data/fasta/Homo_sapiens.GRCh38.dna.primary_assembly.fa',
                     help='Genome .fa file path')
-parser.add_argument('--bigwigs_dir', type=str, default='/data/wangding/nature_meth/', help='BigWigs directory')
+parser.add_argument('--bigwigs_dir', type=str, default=pdir + '/data/bigwigs/', help='BigWigs directory')
 parser.add_argument('--bigwigs_files', type=str, default=['GSM5652317_Blood-B-Z000000UB.hg38.bigwig'], nargs='+', help='BigWigs file')
 parser.add_argument('--cpg_cls_bin_width', type=int, default=5,
                     help='Bin width for CpG count classification, default: 5')
@@ -54,7 +56,7 @@ parser.add_argument('--lr', type=float, default=0.001, help='Learning rate, defa
 parser.add_argument('--meth_pre_process_func', type=str, default='None',
                     help='Method to preprocess, default: None')
 parser.add_argument('--train_hour', type=int, default=99, help='Max train hour, default: 99')
-parser.add_argument('--use_swanlab', action='store_true', help='Use SWAN Lab, default: False. If true, will use swanlab for logging instead of wandb.')
+
 # one_stage_ckpt
 parser.add_argument('--one_stage_ckpt', type=str, default=None,
                     help='Path to the one stage model checkpoint, default: None. If provided, will use this checkpoint for the 2 stage of training.')
@@ -65,15 +67,11 @@ parser.add_argument('--test' , action='store_true', help='Test mode, default: Fa
 # stop_at_step default 50_0000
 parser.add_argument('--stop_at_step', type=int, default=50_0000, help='Stop at step, default: 50_0000. If set, will stop training at this step.')
 
-args = parser.parse_args()
-os.environ["CUDA_VISIBLE_DEVICES"] = f"{args.gpu}"
-args.bigwigs_dir = '/data/wangding/meth_merged_final'
-args.bigwigs_files = ['Adipocytes.merged.hg38.bigwig', 'Endothelium_Pancreas_Islet.merged.hg38.bigwig', 'Macrophages_Liver.merged.hg38.bigwig', 'Blood_B_General.merged.hg38.bigwig', 'Endothelium_Pancreas.merged.hg38.bigwig', 'Macrophages_Lung_Alveolar.merged.hg38.bigwig', 'Blood_B_Memory.merged.hg38.bigwig', 'Endothelium_Saphenous_Vein.merged.hg38.bigwig', 'Macrophages_Lung_Interstitial.merged.hg38.bigwig', 'Blood_Granulocytes.merged.hg38.bigwig', 'Epithelial_Endometrium.merged.hg38.bigwig', 'Muscle_Cardiomyocytes.merged.hg38.bigwig', 'Blood_Monocytes.merged.hg38.bigwig', 'Epithelial_Esophagus.merged.hg38.bigwig', 'Muscle_Skeletal.merged.hg38.bigwig', 'Blood_NK.merged.hg38.bigwig', 'Epithelial_Fallopian.merged.hg38.bigwig', 'Muscle_Smooth_Aorta.merged.hg38.bigwig', 'Blood_T_CD3_General.merged.hg38.bigwig', 'Epithelial_Larynx.merged.hg38.bigwig', 'Muscle_Smooth_Bladder.merged.hg38.bigwig', 'Blood_T_CD4_Central_Memory.merged.hg38.bigwig', 'Epithelial_Ovary.merged.hg38.bigwig', 'Muscle_Smooth_Bronchus.merged.hg38.bigwig', 'Blood_T_CD4_Effector_Memory.merged.hg38.bigwig', 'Epithelial_Pharynx.merged.hg38.bigwig', 'Muscle_Smooth_Coronary.merged.hg38.bigwig', 'Blood_T_CD4_General.merged.hg38.bigwig', 'Epithelial_Skin_Keratinocytes.merged.hg38.bigwig', 'Muscle_Smooth_Prostate.merged.hg38.bigwig', 'Blood_T_CD4_Naive.merged.hg38.bigwig', 'Epithelial_Tongue_Base.merged.hg38.bigwig', 'Neuron_Cerebellum.merged.hg38.bigwig', 'Blood_T_CD8_Effector_Memory.merged.hg38.bigwig', 'Epithelial_Tongue.merged.hg38.bigwig', 'Neuron_Cortex.merged.hg38.bigwig', 'Blood_T_CD8_Effector.merged.hg38.bigwig', 'Epithelial_Tonsil_Palatine.merged.hg38.bigwig', 'Neuron_General.merged.hg38.bigwig', 'Blood_T_CD8_General.merged.hg38.bigwig', 'Epithelial_Tonsil_Pharyngeal.merged.hg38.bigwig', 'Oligodendrocytes.merged.hg38.bigwig', 'Blood_T_CD8_Naive.merged.hg38.bigwig', 'Fibroblasts_Colon.merged.hg38.bigwig', 'Pancreas_Acinar.merged.hg38.bigwig', 'BoneMarrow_Erythrocyte_Progenitor.merged.hg38.bigwig', 'Fibroblasts_Dermal.merged.hg38.bigwig', 'Pancreas_Alpha.merged.hg38.bigwig', 'Bone_Osteoblasts.merged.hg38.bigwig', 'Fibroblasts_Heart.merged.hg38.bigwig', 'Pancreas_Beta.merged.hg38.bigwig', 'Endothelium_Aorta.merged.hg38.bigwig', 'Kidney_Epithelial_Glomerular.merged.hg38.bigwig', 'Pancreas_Delta.merged.hg38.bigwig', 'Endothelium_Kidney_Glomerular.merged.hg38.bigwig', 'Kidney_Epithelial_Tubular.merged.hg38.bigwig', 'Pancreas_Duct.merged.hg38.bigwig', 'Endothelium_Kidney_Tubular.merged.hg38.bigwig', 'Kidney_Podocytes.merged.hg38.bigwig', 'Thyroid_Epithelial.merged.hg38.bigwig', 'Endothelium_Liver.merged.hg38.bigwig', 'Liver_Hepatocytes.merged.hg38.bigwig', 'Endothelium_Lung_Alveolar.merged.hg38.bigwig', 'Macrophages_Colon.merged.hg38.bigwig']
 
-import torch
 from stateless import sigmoid_first, get_bigwig_filepaths, check_args, config_model_loss_fn, load_ckpt, print_model_info, save_model, get_pre_process_func
+args = parser.parse_args()
 args = check_args(args)
-
+# os.environ["CUDA_VISIBLE_DEVICES"] = f"{args.gpu}"
 from models import Melody
 from selene_sdk.sequences import Genome
 from selene_util import GenomicSignalFeatures, RandomPositions, RandomPositionsSampler, SamplerDataLoader
